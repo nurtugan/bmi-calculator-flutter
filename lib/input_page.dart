d0f26d6156f0d720af0b688dab95901f29d8de56
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-11-21 16:32:09 +0600

    Working with SliderTheme class to style the Slider component

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -91,19 +91,29 @@ class _InputPageState extends State<InputPage> {
                       ),
                     ],
                   ),
-                  Slider(
-                    value: height.toDouble(),
-                    onChanged: (double height) {
-                      setState(
-                        () {
-                          this.height = height.round();
-                        },
-                      );
-                    },
-                    min: 120.0,
-                    max: 220.0,
-                    activeColor: Color(0xFFEB1555),
-                    inactiveColor: Color(0xFF8D8E98),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      activeTrackColor: Colors.white,
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      onChanged: (double height) {
+                        setState(
+                          () {
+                            this.height = height.round();
+                          },
+                        );
+                      },
+                      min: 120.0,
+                      max: 220.0,
+                    ),
                   ),
                 ],
               ),
